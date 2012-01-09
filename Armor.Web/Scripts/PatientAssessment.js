b2764d915650d172e9c611d6aa5a0f64717e3c21
Chris Brooker <cbrooker@gmail.com>
2012-01-08 22:04:46 -0500

    Paged 2 of the 3 forms. Fixed a bunch of things

--- Armor.Web/Scripts/PatientAssessment.js
+++ Armor.Web/Scripts/PatientAssessment.js
@@ -11,13 +11,44 @@ $(function () {
 
     //Show and hide the sections as a user progresses through the survey.
     $("#Section1Button").bind("click", function (e) {
-        $("#Section1").slideUp();
-        $("#Section2").slideDown();
+        //$("#Section1").slideUp();
+        //$("#Section2").slideDown();
+
+        //Slide Section1 out
+        var $marginLefty = $("#Section1");
+        $marginLefty.animate({
+            marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 0 ? -$marginLefty.outerWidth() : 0
+        });
+        $marginLefty.hide();
+
+        //Slide Section2 in
+        var $marginLefty2 = $("#Section2");
+        $marginLefty2.show();
+        $marginLefty2.animate({
+            marginLeft: parseInt($marginLefty2.css('marginLeft'), 10) == 0 ? $marginLefty2.outerWidth() : 0
+        });
+
+
     });
 
     $("#Section2Button").bind("click", function (e) {
-        $("#Section2").slideUp();
-        $("#Section3").slideDown();
+        //        $("#Section2").slideUp();
+        //        $("#Section3").slideDown();
+
+        //Slide Section2 out
+        var $marginLefty = $("#Section2");
+        $marginLefty.animate({
+            marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 0 ? -$marginLefty.outerWidth() : 0
+        });
+        $marginLefty.hide();
+
+        //Slide Section3 in
+        var $marginLefty2 = $("#Section3");
+        $marginLefty2.show();
+        $marginLefty2.animate({
+            marginLeft: parseInt($marginLefty2.css('marginLeft'), 10) == 0 ? $marginLefty2.outerWidth() : 0
+        });
+
     });
 
 
---
+++ Armor.Web/Scripts/PreAssessment.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+
+    //Show and hide the sections as a user progresses through the survey.
+    $("#Section1Button").bind("click", function (e) {
+        //Slide Section1 out
+        var $marginLefty = $("#Section1");
+        $marginLefty.animate({
+            marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 0 ? -$marginLefty.outerWidth() : 0
+        });
+        $marginLefty.hide();
+
+        //Slide Section2 in
+        var $marginLefty2 = $("#Section2");
+        $marginLefty2.show();
+        $marginLefty2.animate({
+            marginLeft: parseInt($marginLefty2.css('marginLeft'), 10) == 0 ? $marginLefty2.outerWidth() : 0
+        });
+    });
+
+
+    $("#Section2Button").bind("click", function (e) {
+        //Slide Section2 out
+        var $marginLefty = $("#Section2");
+        $marginLefty.animate({
+            marginLeft: parseInt($marginLefty.css('marginLeft'), 10) == 0 ? -$marginLefty.outerWidth() : 0
+        });
+        $marginLefty.hide();
+
+        //Slide Section3 in
+        var $marginLefty2 = $("#Section3");
+        $marginLefty2.show();
+        $marginLefty2.animate({
+            marginLeft: parseInt($marginLefty2.css('marginLeft'), 10) == 0 ? $marginLefty2.outerWidth() : 0
+        });
+
+    });
+
+});
